2c64f0522fdf745971520bd887456071ec1345f9
Pablo <zerytv75@gmail.com>
2024-02-27 13:36:40 +0000

    footer arreglado + account

--- JavaScript/templateLoader.js
+++ JavaScript/templateLoader.js
@@ -10,23 +10,5 @@ function cargarTemplate(url, selector) {
         .catch(error => {
             console.error('Error al cargar el template:', error);
         });
-    function agregarImagen(contenedor, imagenPath, texto) {
-        let nuevoElemento = document.createElement('div');
-        nuevoElemento.className = 'imagewd';
-        nuevoElemento.innerHTML = `<img src="${imagenPath}" alt=""> ${texto}`;
-        contenedor.appendChild(nuevoElemento);
-    }
-
-    // Datos de imágenes y textos
-    const imagenesYTextos = [
-        { imagenPath: '/Images/aspa.png', texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed' },
-        // Agrega más objetos según sea necesario
-    ];
-
-    // Agregar imágenes y textos al contenedor
-    const contenedorImagenes = document.getElementById('imagenContainer');
-    imagenesYTextos.forEach(item => {
-        agregarImagen(contenedorImagenes, item.imagenPath, item.texto);
-    });
 }
 
