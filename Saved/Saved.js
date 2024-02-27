d3957a35d76e2eb8e5b908c2db87f1307cfc7218
Jorge Álamo <jorgealamosan@gmail.com>
2024-02-27 11:20:00 +0000

    prueba

--- Saved/Saved.js
+++ Saved/Saved.js
@@ -1,11 +1,11 @@
-// Utilizamos fetch para obtener el contenido de "header.html"
+// Utilizamos fetch para obtener el contenido de "Header.html"
 fetch('../Reusables/Header/header.html')
     .then(response => response.text())
     .then(data => {
         // Insertamos el contenido en el contenedor con id "headerContainer"
         document.getElementById('headerContainer').innerHTML = data;
     })
-    .catch(error => console.error('Error al cargar el archivo header.html:', error));
+    .catch(error => console.error('Error al cargar el archivo Header.html:', error));
 
 fetch('../Reusables/Footer/footer.html')
     .then(response => response.text())
