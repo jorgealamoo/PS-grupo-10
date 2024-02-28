066e3e9551cfdc3c5826636306612f962b7dd55b
Pablo <zerytv75@gmail.com>
2024-02-28 09:15:31 +0000

    Saved arreglado

--- JavaScript/imagenLoader.js
+++ JavaScript/imagenLoader.js
@@ -1,16 +1,25 @@
-// imagenLoader.js
 function agregarImagen(contenedor, imagenPath, texto) {
     let nuevoElemento = document.createElement('div');
     nuevoElemento.className = 'imagewd';
-    nuevoElemento.innerHTML = `<img src="${imagenPath}" alt=""> ${texto}`;
+
+    let imagen = document.createElement('img');
+    imagen.src = imagenPath;
+
+    imagen.style.width = '450px';
+    imagen.style.height = '450px';  // Ajusta la altura de forma proporcional
+
+    nuevoElemento.appendChild(imagen);
+    nuevoElemento.innerHTML += texto;
+
     contenedor.appendChild(nuevoElemento);
 }
 
 function cargarImagenes() {
     const contenedorImagenes = document.getElementById('imagenContainer');
     const imagenesYTextos = [
-        { imagenPath: '/Images/aspa.png', texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed'},
+        { imagenPath: '/Images/aspa.png', texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed'},
         { imagenPath: '/Images/aspa2.png', texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed' },
+        { imagenPath: '/Images/img.png', texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed' },
     ];
 
     imagenesYTextos.forEach(item => {
