b36a73233a40fe1b8ecf1c7b9cb45d43a6a0257f
Javier Ocón <Javier.obarreiro@gmail.com>
2024-03-13 10:58:34 +0000

    Implement Google Login

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
 // index.js
 
-import { signInWithGoogle } from './firebaseAuthentication.js';
+import { signInWithGoogle } from './auth.js';
 
-document.getElementById('googleLoginButton').addEventListener('click', async function() {
+document.getElementById('googleLogin').addEventListener('click', async function() {
     try {
         const user = await signInWithGoogle();
         console.log('El usuario ha iniciado sesión correctamente:', user);
@@ -11,21 +10,5 @@ document.getElementById('googleLoginButton').addEventListener('click', async fun
     } catch (error) {
         console.error('Error al iniciar sesión con Google:', error);
         alert('Error al iniciar sesión con Google: ' + error.message);
-    }
-});
-
-import { logInUserPassword} from './firebaseAuthenticationUserPassword.js';
-
-document.getElementById('Login').addEventListener('click', async function() {
-
-    try {
-        const email = document.getElementById('user_name').value;
-        const password = document.getElementById('password').value;
-        const user = await logInUserPassword(email, password);
-        console.log('El usuario ha iniciado sesión correctamente:', user);
-        alert('Inicio de sesión exitoso con usuario y contraseña');
-    } catch (error) {
-        console.error('Error al iniciar sesión con usuario y contraseña:', error);
-        alert('Error al iniciar sesión con usuario y contraseña: ' + error.message);
     }
 });
