a588067366f20fa3a190a6f178845446c1e5a85d
Javier Ocón <Javier.obarreiro@gmail.com>
2024-03-15 21:44:48 +0000

    Facebook Login

--- src/auth.js
+++ src/auth.js
@@ -1,11 +1,24 @@
 // auth.js
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 import { app } from './firebaseConfig.js'; // Importa la instancia de la app de Firebase desde otro archivo
 
 export async function signInWithGoogle() {
     try {
         const auth = getAuth(app);
         const provider = new GoogleAuthProvider();
+        const result = await signInWithPopup(auth, provider);
+        localStorage.setItem('userId', result.user.uid);
+        localStorage.setItem('userEmail', result.user.email);
+        return result.user;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function signInWithFacebook() {
+    try {
+        const auth = getAuth(app);
+        const provider = new FacebookAuthProvider();
         const result = await signInWithPopup(auth, provider);
         localStorage.setItem('userId', result.user.uid);
         localStorage.setItem('userEmail', result.user.email);
