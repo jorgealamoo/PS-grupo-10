fda520d7476d5b1f0b4359d90ecdb506462f45f1
Javier Sánchez Godoy <81640937+Yaivi@users.noreply.github.com>
2024-03-15 17:36:55 +0000

    Funciones importadas de index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,50 @@
 // index.js
-import {signInWithGoogle} from './auth.js';
 
-document.getElementById('googleLogin').addEventListener('click', async function() {
-    try {
-        const user = await signInWithGoogle();
-        console.log('El usuario ha iniciado sesión correctamente:', user);
-        alert('Inicio de sesión exitoso con Google');
-    } catch (error) {
-        console.error('Error al iniciar sesión con Google:', error);
-        alert('Error al iniciar sesión con Google: ' + error.message);
-    }
-});
+import { signInWithGoogle } from './auth.js';
 
-
+export async function  logGoogle() {
+    signInWithGoogle()
+        .then(user => {
+            // Si la autenticación es exitosa, aquí puedes realizar otras acciones, como redirigir a otra página
+            console.log('El usuario ha iniciado sesión correctamente:', user);
+            alert('Inicio de sesión exitoso con Google');
+        })
+        .catch(error => {
+            // Manejar errores de autenticación
+            console.error('Error al iniciar sesión con Google:', error);
+            alert('Error al iniciar sesión con Google: ' + error.message);
+        });
+}
 
 import {logInUserPassword} from "./authUserPassword.js";
 
-document.getElementById('LogIn').addEventListener('click',async function() {
-    try {
+export async function logUserContraseña() {
+
         const email = document.getElementById('user_name').value;
-        console.log(email)
         const password = document.getElementById('password').value;
-        console.log(password)
 
-        const user = await logInUserPassword(email,password);
-        console.log('El usuario ha iniciado sesión correctamente con su email:', user);
-        location.href='/PS-grupo-10/Front_end/MapPage/map.html'
-    } catch (error) {
-        console.error('Error al iniciar sesión con email:', error);
-        alert('Error al iniciar sesión con email: ' + error.message);
+        logInUserPassword(email,password)
+            .then(user =>{
+                console.log('El usuario ha iniciado sesión correctamente con su email:', user);
+                alert('Inicio de sesión exitoso con email');
+            }).catch (error=>{
+            console.error('Error al iniciar sesión con email:', error);
+            alert('Error al iniciar sesión con email: ' + error.message);
+        });
     }
-});
 
+import {SignUp} from "./signUp.js";
+export async function crearUsuario() {
+    const emailNuevo = document.getElementById('email').value;
+    const passwordNuevo = document.getElementById('password').value;
 
-import {changePasswordUser} from "./changePasswd.js";
+    SignUp(emailNuevo,passwordNuevo)
+        .then(user =>{
+            console.log('El usuario ha creado correctamente su cuenta:', user);
+            alert('Creación de cuenta exitosa');
+        }).catch (error=>{
+            console.error('Error al crear la cuenta:', error);
+        alert('Error al crear la cuenta: ' + error.message);
+
+    });
+}
