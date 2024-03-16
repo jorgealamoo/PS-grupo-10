057fcf7b848010ec51f1518c03eb47fc3e1f9d61
Pedro Manuel <pedrom.rodriguezhernandez@gmail.com>
2024-03-16 12:37:41 +0000

    dsa

--- Front_end/JavaScript/UploadImages.js
+++ Front_end/JavaScript/UploadImages.js
@@ -1,9 +1,30 @@
-async function uploadImageToAPI(image) {
+async function uploadAllImagesToAPI(imageList) {
+    const successfulUploads = [];
+
+    // Iterar sobre cada imagen en la lista
+    for (const imageDataURL of imageList) {
+        try {
+            // Cargar la imagen a la API
+            const uploadedImageName = await uploadImageToAPI(imageDataURL);
+            if (uploadedImageName !== null) {
+                successfulUploads.push(uploadedImageName);
+            }
+        } catch (error) {
+            console.error('Error al subir una imagen:', error.message);
+        }
+    }
+    return successfulUploads;
+}
+
+async function uploadImageToAPI(imageDataURL) {
     const url = 'http://localhost:3000/api/uploadImage';
     const formData = new FormData();
 
+    // Convertir el Data URL en un Blob
+    const blob = dataURLtoBlob(imageDataURL);
+
     // Agregar la imagen al FormData
-    formData.append('file', image, image.name);
+    formData.append('file', blob, 'image.png'); // Puedes ajustar el nombre del archivo según tu preferencia
 
     try {
         const response = await fetch(url, {
@@ -12,7 +33,7 @@ async function uploadImageToAPI(image) {
         });
 
         if (!response.ok) {
-            throw new Error('Error al subir la imagen ' + image.name + ': ' + response.statusText);
+            throw new Error('Error al subir la imagen: ' + response.statusText);
         }
 
         // Parsear la respuesta para obtener el nombre de la imagen
@@ -22,26 +43,21 @@ async function uploadImageToAPI(image) {
         return imageName;
     } catch (error) {
         // Manejar errores
-        console.error('Error al subir la imagen ' + image.name + ': ' + error.message);
+        console.error('Error al subir la imagen:', error.message);
         return null;
     }
 }
 
-async function uploadAllImagesToAPI(images) {
-    const imagesToUpload = Object.values(images);
-    const successfulUploads = [];
-
-    for (const image of imagesToUpload) {
-        try {
-            const imageName = await uploadImageToAPI(image);
-            if (imageName !== null) {
-                successfulUploads.push(imageName);
-            }
-        } catch (error) {
-            console.error('Error al subir una imagen:', error.message);
-        }
+// Función para convertir un Data URL en un Blob
+function dataURLtoBlob(dataURL) {
+    const arr = dataURL.split(',');
+    const mime = arr[0].match(/:(.*?);/)[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
     }
-    return successfulUploads;
+    return new Blob([u8arr], { type: mime });
 }
-
-export {uploadAllImagesToAPI};
+export { uploadAllImagesToAPI };
