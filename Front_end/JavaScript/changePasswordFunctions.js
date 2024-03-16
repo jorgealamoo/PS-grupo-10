bf2486a4904bd4c7e59f67b54441b515fee5c809
Javier Ocón <Javier.obarreiro@gmail.com>
2024-03-16 11:19:42 +0000

    Change Password Function

--- Front_end/JavaScript/changePasswordFunctions.js
+++ Front_end/JavaScript/changePasswordFunctions.js
@@ -6,34 +6,49 @@ function toggleChangePassword() {
 }
 
 async function validateOfChangePassword() {
+    const userID = localStorage.getItem('userId');
     const pass1 = document.getElementById("newPassword");
+    console.log(pass1.value);
     const pass2 = document.getElementById("repeatPassword");
+    console.log(pass2.value);
     var errorDiv = document.getElementById("passwordError");
 
     if (pass1.value !== pass2.value) {
         errorDiv.innerText = "Passwords do not match";
     } else if (pass1.value === "" && pass2.value === "") {
         errorDiv.innerText = "Password is empty";
+    } else if (pass1.value.length < 6){
+        errorDiv.innerText = "At least 6 characters";
+        return;
     } else {
-        //errorDiv.innerText = "";
-        //toggleChangePassword();
-        console.log('Hace ALGO')
+        console.log('Contraseñas Iguales')
+        errorDiv.innerText = "";
         try {
-            if (document.getElementById('newPassword').value != document.getElementById('newPassword').value) {
-                alert("No coinciden las contraseñas")
-                return; //Se sale si no coinciden las contraseñas
+            try {
+                const response = await fetch('http://localhost:3000/api/changePassword', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ "userId": userID.toString(), "newPassword" :pass1.value.toString() })
+                });
+
+                console.log(JSON.stringify({ "userId": userID.toString(), "newPassword" :pass1.value.toString()}))
+
+                const data = await response.json();
+                if (response.ok) {
+                    alert(data.message);
+                    toggleChangePassword();
+                } else {
+                    document.getElementById('message').textContent = data.error.message;
+                    alert('Error al cambiar contraseña: ' + data.error.message);
+                }
+            } catch (error) {
+                console.error('Error during password change:', error.message);
+                alert('Error: ' + error.message);
             }
 
-            const newPassword = document.getElementById('newPassword').value;
-            console.log(document.getElementById('newPassword').value);
-            const user = localStorage.getItem('userId');
-            console.log(localStorage.getItem('userId'));
 
-            //Se cambia la contraseña
-            /*if (await changePassword(user, newPassword)) {
-                alert('Se ha cambiado la contraseña');
-                console.error('Se ha cambiado la contraseña');
-            }*/
         } catch (error) {
             console.error('Error al iniciar sesión con Google:', error);
             alert('Error al iniciar sesión con Google: ' + error.message);
