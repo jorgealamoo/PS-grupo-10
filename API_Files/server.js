9f27db6fd8a8e68dfe75e838bd4bcdff63480a2f
Javier Ocón <Javier.obarreiro@gmail.com>
2024-03-16 12:35:36 +0000

    API Files

--- API_Files/server.js
+++ API_Files/server.js
@@ -18,13 +18,7 @@ app.use(express.json());
 
 // Initialize Firebase app
 const firebaseConfig = {
-    apiKey: "AIzaSyBbGLFzzUj5y8F927mW6JcrObKnF9bynAY",
-    authDomain: "proyecto-ps-22ec6.firebaseapp.com",
-    projectId: "proyecto-ps-22ec6",
-    storageBucket: "proyecto-ps-22ec6.appspot.com",
-    messagingSenderId: "611131563975",
-    appId: "1:611131563975:web:f5663ec606ee9eecf003e2",
-    measurementId: "G-BJ8S3HC2XC"
+    //..................
 };
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
