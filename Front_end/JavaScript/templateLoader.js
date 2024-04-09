e5463f73056ebdd2c4ea35746425c994f3e2e1d5
Pablo <zerytv75@gmail.com>
2024-04-09 12:38:17 +0100

    Header guapeton

--- Front_end/JavaScript/templateLoader.js
+++ Front_end/JavaScript/templateLoader.js
@@ -6,7 +6,18 @@ function cargarTemplate(url, selector) {
         .then(data => {
             let container = document.querySelector(selector);
             container.innerHTML = data;
-            loadProfilePhoto("ProfileImageHEADER");
+            if (selector === "#desktopHeader") {
+                loadProfilePhoto("ProfileImageHEADER");
+                let currentPageUrl = window.location.href;
+                currentPageUrl = ".." + currentPageUrl.substring(44);
+                const navLinks = document.querySelectorAll("#nav-bar a");
+                navLinks.forEach(function (link) {
+                    if (link.getAttribute("href") === currentPageUrl) {
+                        link.style.textDecoration = "underline";
+                        link.style.color = "green";
+                    }
+                });
+            }
         })
         .catch(error => {
             console.error('Error al cargar el template:', error);
