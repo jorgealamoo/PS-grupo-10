cdc633924c83a23a23a0ca052b2d037c243550b6
pedro manuel <pedrom.rodriguezhernandez@gmail.com>
2024-04-29 19:34:12 +0100

    Tema de listas

--- Front_end/JavaScript/cargarTodasLasPublicaciones.js
+++ Front_end/JavaScript/cargarTodasLasPublicaciones.js
@@ -1,31 +1,52 @@
-async function loadAllPublication() {
-    const  response = await fetch('http://localhost:3000/api/fetchData/publicacion/')
-        .then(response =>{
-            if(!response.ok){
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        });
-    return response;
+async function loadAllPublication(collection) {
+    const response = await fetch('http://localhost:3000/api/fetchData/'+collection+'/');
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
 }
 
-async function loadMapInToList(publicationMap) {
-    const response = await fetch(`http://localhost:3000/api/changeDoc/listas/publicaciones`, {
+async function loadMapInToList(document,cosas) {
+    await fetch('http://localhost:3000/api/changeDoc/listas/'+document, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(publicationMap)
+        body: JSON.stringify(cosas)
     });
 }
 
-async function updatePublicationList(){
-    let publicationMap = {}
-    const publications = await loadAllPublication();
+async function updatePublicationList(collection,document,from) {
+    const publicationMap = {};
+    const publications = await loadAllPublication(from);
     for (const publication of publications) {
-        publicationMap[publication.publication_id] = publication.nombre;
+        const id = publication.publication_id;
+        const nombre = publication.nombre;
+        if (nombre in publicationMap) {
+            publicationMap[nombre].push(id);
+        } else {
+            publicationMap[nombre] = [id];
+        }
     }
-    await loadMapInToList({publicaciones:publicationMap});
+    await loadMapInToList(document,publicationMap);
+}
+export async function addToList(document, name, id) {
+    let data = await fetch('http://localhost:3000/api/getDocument/listas/' + document)
+        .then(response =>{
+            if (!response){
+                throw new Error("no se ha podido encontrar la lista");
+            }else {
+                return response.json();
+            }
+        });
+    if (data.hasOwnProperty(name)){
+        data[name].push(id);
+    }else{
+        data[name] = [id];
+    }
+    await loadMapInToList(document,data);
 }
 
-updatePublicationList();
+
+await updatePublicationList("listas","publicaciones", "publicacion");
+
